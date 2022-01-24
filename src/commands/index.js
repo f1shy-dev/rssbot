7dbd02cd7a63a363fa3d11e05829092556063d2e
f1shy-dev <agarwal.vrishank@gmail.com>
2022-01-24 18:36:30 +0000

    random number command:

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,12 +2,14 @@ import { ping } from './ping'
 import { reddit } from './reddit'
 import { help } from './help'
 import { debug } from './debug'
+import { random } from './random'
 
-export default { help, ping, reddit, debug }
+export default { help, ping, reddit, debug, random }
 
 export const descMap = {
     help: 'shows this help menu',
     ping: '🏓 pong!',
     reddit: 'view posts from reddit',
     debug: 'shows debug statistics',
+    random: 'generates a random number',
 }
---
+++ src/commands/random.js
@@ -0,0 +1,14 @@
+import { textData } from '../util/botData'
+
+export const random = (m, a, c) => {
+    const start = parseInt(a[0])
+    const end = parseInt(a[1])
+
+    if (isNaN(start) || isNaN(end) || start > end || start < 0 || end < 0)
+        return textData(
+            'You must provide 2 valid positive numbers such as below:<br><br><b>!random 1 10</b>'
+        )
+
+    const random = Math.floor(Math.random() * (end - start + 1)) + start
+    return textData(`🎲 Your number: <b>${random}</b>`)
+}
