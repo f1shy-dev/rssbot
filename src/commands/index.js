d1f1a075de831e2fb758fb4785d0952eacb9d885
f1shy-dev <agarwal.vrishank@gmail.com>
2022-01-24 18:10:50 +0000

    register debug

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,10 +2,11 @@ import { ping } from './ping'
 import { reddit } from './reddit'
 import { help } from './help'
 
-export default { help, ping, reddit }
+export default { help, ping, reddit, debug }
 
 export const descMap = {
     help: 'shows this help menu',
     ping: '🏓 pong!',
     reddit: 'view posts from reddit',
+    debug: 'shows debug statistics',
 }
